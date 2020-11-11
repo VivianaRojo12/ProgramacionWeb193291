a27816a839d852ba88aa88aa9485171c6e159a8f
Viviana Rojo <193291@ids.upchiapas.edu.mx>
2020-11-10 22:04:29 -0600

    Add Register Component

--- Angular/src/app/register/register.component.ts
+++ Angular/src/app/register/register.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
 
-  constructor() { }
+export class RegisterComponent implements OnInit {
+  registerFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder, private _router: Router) {
+
+  }
 
   ngOnInit(): void {
+    this.registerFormGroup = this._formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  register(): void {
+    const data = this.registerFormGroup.value;
   }
 
 }
