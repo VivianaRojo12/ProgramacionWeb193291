004bd66a5594bc74a8a698bcfa301b35f31e4870
Viviana Rojo <193291@ids.upchiapas.edu.mx>
2020-11-11 22:16:03 -0600

    Register Front and Back

--- Angular/src/app/register/register.component.ts
+++ Angular/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {AuthServiceService} from '../service/auth/auth-service.service';
 
 import { Router } from '@angular/router';
 
@@ -12,7 +13,7 @@ import { Router } from '@angular/router';
 export class RegisterComponent implements OnInit {
   registerFormGroup: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder, private _router: Router) {
+  constructor(private _formBuilder: FormBuilder, private _router: Router,  private _authServiceService: AuthServiceService) {
 
   }
 
@@ -20,7 +21,7 @@ export class RegisterComponent implements OnInit {
     this.registerFormGroup = this._formBuilder.group({
       username: ['', Validators.required],
       email: ['', Validators.required],
-      password: ['', Validators.required],
+      password1: ['', Validators.required],
       password2: ['', Validators.required]
       
     })
@@ -28,6 +29,14 @@ export class RegisterComponent implements OnInit {
 
   register(): void {
     const data = this.registerFormGroup.value;
+    if (data.username && data.email && data.password1 && data.password2) {
+      this._authServiceService.register(data.username, data.email, data.password1, data.password2).subscribe(access => {
+      this._router.navigate(['login'])
+      }, error => {
+        console.log("Hola")
+      }
+      );
+    }
   }
 
 }
--- Angular/src/app/service/auth/auth-service.service.ts
+++ Angular/src/app/service/auth/auth-service.service.ts
@@ -31,6 +31,15 @@ export class AuthServiceService {
     return this.http.post(`${this.api}api/v1/login/`, { username, password }, httpOptions);
   }
 
+  register(username: string, email: string, password1: string, password2: string): Observable<any> {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+    return this.http.post(`${this.api}api/v1/register/rest-auth/registration/`, { username, email, password1, password2 }, httpOptions);
+  }
+
   getUsers() {
     let user = JSON.parse(localStorage.getItem('user'));
     const httpOptions = {
